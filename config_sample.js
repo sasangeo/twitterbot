a210df01c6647cb114e5663d16b37aaea01ca6e8
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-10-03 21:04:46 +0700

    Update config_sample.js

--- config_sample.js
+++ config_sample.js
@@ -1,8 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'API key',
-    consumer_secret:      'API secret key',
-    access_token:         'Access token',
-    access_token_secret:  'Access token secret'
+    consumer_key:         'e7Mi31kCmKehtEuhslgTRbGJs',
+    consumer_secret:      'rWwPGVxVO05uOdf0tcEIzq5r7cPVP7FtpRmdjfFnVTqniL95rD',
+    access_token:         '1427443084879532035-e9UtpHkDJZbf1eeh1ZyaiLXJFvFal5',
+    access_token_secret:  'JNh92eGWRhnARyfksceR8nBLqKZ2JtrCfftWef4pvOuSj'
   }
   
